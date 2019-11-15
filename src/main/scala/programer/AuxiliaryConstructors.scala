974cc04e08aaeb2f5d65e7f008552663fe662e00
wq <wqkant@gmail.com>
2019-11-15 11:00:08 +0800

    auxiliary constructor

--- src/main/scala/programer/AuxiliaryConstructors.scala
+++ src/main/scala/programer/AuxiliaryConstructors.scala
@@ -1,4 +1,15 @@
 package programer
+//
+//
+//There are several important points to this recipe:
+//
+//Auxiliary constructors are defined by creating methods named this.
+//
+//Each auxiliary constructor must begin with a call to a previously defined constructor.
+//
+//Each constructor must have a different signature.
+//
+//One constructor calls another constructor with the name this.
 
 object AuxiliaryConstructors extends App {
   val p1 = new Pizza(12, "gg")
@@ -36,4 +47,32 @@ class Pizza (var crustSize: Int, var crustType: String) {
 object Pizza {
   val DEFAULT_CRUST_SIZE = 12
   val DEFAULT_CRUST_TYPE = "THIN"
+}
+
+
+// the case class
+case class Person (var name: String, var age: Int)
+
+// the companion object
+object Person {
+
+  def apply() = new Person("<no name>", 0)
+  def apply(name: String) = new Person(name, 0)
+
+}
+
+object CaseClassTest extends App {
+
+  val a = Person()         // corresponds to apply()
+  val b = Person("Pam")    // corresponds to apply(name: String)
+  val c = Person("William Shatner", 82)
+
+  println(a)
+  println(b)
+  println(c)
+
+  // verify the setter methods work
+  a.name = "Leonard Nimoy"
+  a.age = 82
+  println(a)
 }
