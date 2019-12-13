25a007b617396a42cae92c576130fc10765d769b
kant132 <wqkant@gmail.com>
2019-12-14 00:08:59 +0800

    think overwrite after cache

--- build.sbt
+++ build.sbt
@@ -7,7 +7,9 @@ scalaVersion := "2.11.12"
 
 libraryDependencies ++= Seq(
   "org.apache.spark" %% "spark-core" % "2.4.4",
-  "org.apache.spark" %% "spark-sql" % "2.4.4"
+  "org.apache.spark" %% "spark-sql" % "2.4.4",
+  // https://mvnrepository.com/artifact/org.apache.spark/spark-mllib
+  "org.apache.spark" %% "spark-mllib" % "2.4.4"
 )
 
 libraryDependencies += "com.typesafe" % "config" % "1.3.2"
---
+++ src/main/java/ThinkHashMap.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+
+public class ThinkHashMap {
+
+
+    private static HashMap<Integer, String> map = new HashMap<Integer, String>(2, 0.75f);
+
+    public static void main(String[] args) throws InterruptedException {
+        map.put(5, "C");
+
+        new Thread("Thread1") {
+            public void run() {
+                map.put(7, "B");
+                map.put(3, "d");
+                System.out.println(map);
+            }
+
+        }.start();
+        new Thread("Thread2") {
+            public void run() {
+                map.put(3, "A");
+                System.out.println(map);
+            }
+
+        }.start();
+        Thread.sleep(1000);
+        System.out.println(map);
+    }
+
+}
